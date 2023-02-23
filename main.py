7246f0ca5b2e458ec0301fd9a413cba437babc0d
Simon Fortin <siforf@pop-os.localdomain>
2023-02-23 16:14:02 -0500

    update event structure

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ from subprocess import Popen, PIPE, STDOUT
 
 def handler(event, context):
     notebook = event['notebook_path']
+    arg = event['launcher_arg']
+
     file = os.path.join('/tmp', os.path.basename(notebook).replace('.ipynb', '.py'))
     os.system('jupyter nbconvert --to python %s --output %s' % (notebook, file))
     cwd = os.path.dirname(notebook)
-    if type(event['arg']) == tuple:
-        command_list = ['python', file] +list(event['arg'])
-    else:
-        command_list = ['python', file] +[event['arg']]
+
+    command_list = ['python', file] +[arg['scenario']]
 
     my_env = os.environ.copy()
     my_env['PYTHONPATH'] = os.pathsep.join(sys.path)
     process = Popen(command_list, stdout=PIPE, stderr=STDOUT, env=my_env, cwd=cwd)
     process.wait()
-    print(process.stdout.read())
+
+    content = process.stdout.read()
+    print(content)
+
+    if 'Error' in content and "end_of_notebook" not in content:
+        return 1
 
     return 0
