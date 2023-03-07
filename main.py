5774c29d30ed52b97372f23ab8ae8d75d3503ed1
Simon Fortin <siforf@DESKTOP-PD0U2PV>
2023-03-07 17:26:22 -0500

    Training_folder as input to notebooks

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import sys
 import os
+import json
 import boto3
-import time
+import shutil
 
 sys.path.insert(0, os.path.abspath('quetzal'))
 from subprocess import Popen, PIPE, STDOUT
-import subprocess
 
 s3 = boto3.resource('s3') 
 
-def download_s3_folder(bucket_name, s3_folder, local_dir=None):
+def download_s3_folder(bucket_name, s3_folder, local_dir='/tmp'):
     """
     Download the contents of a folder directory
     Args:
@@ -27,19 +27,50 @@ def download_s3_folder(bucket_name, s3_folder, local_dir=None):
             continue
         bucket.download_file(obj.key, target)
 
+
+def upload_s3_folder(bucket_name, folder, local_dir='/tmp'):
+    """
+    Upload the contents of a folder directory to S3
+    Args:
+        bucket_name: the name of the s3 bucket
+        s3_folder: the folder path in the s3 bucket
+        local_dir: a relative or absolute directory path in the local file system
+    """
+    bucket = s3.Bucket(bucket_name)
+    for root, _, files in os.walk(local_dir):
+        for file in files:
+            local_path = os.path.join(root, file)
+            s3_path = os.path.join(folder, os.path.relpath(root, local_dir), file)
+            bucket.upload_file(local_path, s3_path)
+
+def clean_folder(folder='/tmp'):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
 def handler(event, context):
     bucket_name = os.environ['BUCKET_NAME']
     
-    download_s3_folder(bucket_name, 'dev', local_dir='/tmp')
-
+    # Move (and download) model data and inputs to ephemeral storage
+    clean_folder() # Clean ephemeral storage
+    shutil.copytree('./inputs', '/tmp/inputs')
+    download_s3_folder(bucket_name, 'dev')
+    
     notebook = event['notebook_path']
-    arg = event['launcher_arg']
+    arg = json.dumps(event['launcher_arg'])
 
     file = os.path.join('/tmp', os.path.basename(notebook).replace('.ipynb', '.py'))
     os.system('jupyter nbconvert --to python %s --output %s' % (notebook, file))
     cwd = os.path.dirname(notebook)
 
-    command_list = ['python', file] +[arg['scenario']]
+    command_list = ['python', file, arg]
 
     my_env = os.environ.copy()
     my_env['PYTHONPATH'] = os.pathsep.join(sys.path)
@@ -51,10 +82,9 @@ def handler(event, context):
 
     if 'Error' in content and "end_of_notebook" not in content:
         raise RuntimeError("Error on execution")
-
-    sync_command = ['aws', 's3', 'sync', '/tmp/dev/', f's3://{bucket_name}/dev/']  
-    process = subprocess.run(sync_command, stdout=PIPE, stderr=STDOUT)
-    content = process.stdout.read().decode("utf-8")
-    print(content)
+    
+    os.remove(file)
+    shutil.rmtree('/tmp/inputs')
+    upload_s3_folder(bucket_name, 'dev')
 
     return event
